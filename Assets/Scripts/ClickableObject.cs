00baf7890cd2a5b2541b90cb6d5540b537ee3da4
Tyler Wayne <twayne@captechventures.com>
2015-09-30 22:09:41 -0400

    Messing around with functions and global stuff

---
+++ Assets/Scripts/ClickableObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickableObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	//On click event
+	void OnMouseDown() {
+		print ("Clicked this object!");
+
+	}
+}
---
+++ Assets/Scripts/GlobalContext.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GlobalContext : MonoBehaviour {
+
+	public static bool Paused = false;
+	public static DateTime? TotalTimePlayed;
+
+	void Awake() {
+		if (GlobalContext.TotalTimePlayed == null) {
+			GlobalContext.TotalTimePlayed = DateTime.Now;
+		}
+		DontDestroyOnLoad (transform.gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		((DateTime)GlobalContext.TotalTimePlayed).AddDays (1);
+		print (((DateTime)GlobalContext.TotalTimePlayed).ToShortDateString());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
